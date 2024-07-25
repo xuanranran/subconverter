206bfbc7f7172ec1b38a5f0bf653551860639dab
LM-Firefly <firefly.lzh@gmail.com>
2024-07-25 16:48:35 +0800

    Add SimpleWeb

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## *** 📜 CHANGELOG 📜 ***
+
+###  💫 Just have fun, nothing has changed. 
+### 📝 Check the Full Changelog for more information. 
---
+++ base/sub-web/js/precache-manifest.7cc9c3d9f5ed3d579591dbd0af860215.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd7d7b6923016ad755aed91ed7ed31",
+    "url": "_redirects"
+  },
+  {
+    "revision": "93a421c8e48c87890703",
+    "url": "css/app.d04bb92b.css"
+  },
+  {
+    "revision": "55c4f71efe3c5af8cc3a",
+    "url": "css/chunk-vendors.636ec213.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "88209a3479a6699fb6790bcfa6e6ea7f",
+    "url": "index.html"
+  },
+  {
+    "revision": "93a421c8e48c87890703",
+    "url": "js/app.1ceb19ca.js"
+  },
+  {
+    "revision": "cc5b1581652aa4815a24",
+    "url": "js/chunk-22fdbc3c.d882deb2.js"
+  },
+  {
+    "revision": "55c4f71efe3c5af8cc3a",
+    "url": "js/chunk-vendors.513a2294.js"
+  },
+  {
+    "revision": "3fa7348f187dae70fad03ee4cc4e0a4f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
