5ee210a763a696b7112b9101b9e9a000511b46aa
LovinYarn <juanshengyuan@gmail.com>
2024-10-06 21:23:34 +0800

    Revert "Merge branch 'main' of https://github.com/xuanranran/subconverter"
    This reverts commit 5c7053781fcf71572decf0bf10102bd69eceda11, reversing
    changes made to 6cc3d1fab3de7f7fcf6e398ee367a9b196d32ed3.

--- src/config/binding.h
+++ src/config/binding.h
@@ -64,16 +64,6 @@ namespace toml
             case "ssid"_hash:
                 conf.Type = ProxyGroupType::SSID;
                 break;
-            case "smart"_hash:
-                conf.Type = ProxyGroupType::Smart;
-                conf.Url = toml::find<String>(v, "url");
-                conf.Interval = toml::find<Integer>(v, "interval");
-                conf.Tolerance = toml::find_or<Integer>(v, "tolerance", 0);
-                if(v.contains("lazy"))
-                    conf.Lazy = toml::find_or<bool>(v, "lazy", false);
-                if(v.contains("evaluate-before-use"))
-                    conf.EvaluateBeforeUse = toml::find_or(v, "evaluate-before-use", conf.EvaluateBeforeUse.get());
-                break;
             default:
                 throw toml::syntax_error("Proxy Group has incorrect type, should be one of following:\n  select, url-test, load-balance, fallback, relay, ssid", v.at("type").location());
             }
@@ -229,9 +219,6 @@ namespace INIBinding
                     break;
                 case "ssid"_hash:
                     conf.Type = ProxyGroupType::SSID;
-                    break;
-                case "smart"_hash:
-                    conf.Type = ProxyGroupType::Smart;
                     break;
                 default:
                     continue;
